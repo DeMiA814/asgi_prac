425b26b1e3a85ee2ef178d965d33eaa25e642ff5
yusuke <y.tosa.kyoto@gmail.com>
2020-05-02 21:54:20 +0900

    簡易的なインターフェース実装

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    result = ""
+    if request.method == "GET":
+        if request.GET.get("sub") == "ping":
+            result = "pong!"
+    return render(request, "myapp/index.html", {'result': result})
